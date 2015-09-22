9b2279814ea2f9cb5d3f1564dbcc6e0548ccf5dd
aaronistheman <aarons.7007@gmail.com>
2015-09-21 21:57:05 -0700

    Created function that generates the fifty pairs of state and capital city

--- app/src/main/java/com/aaronistheman/statecapitals/GameActivity.java
+++ app/src/main/java/com/aaronistheman/statecapitals/GameActivity.java
@@ -7,6 +7,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameActivity extends Activity implements
         View.OnClickListener {
 
@@ -16,11 +19,20 @@ public class GameActivity extends Activity implements
     Button choice4;
     TextView stateName;
 
+    // The first String is the state;
+    // the second String is the capital of the state
+    Map<String, String> stateCapitalMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        setUpButtons();
+        setUpStateCapitalMap();
+    }
+
+    private void setUpButtons() {
         choice1 = (Button) findViewById(R.id.buttonChoice1);
         choice2 = (Button) findViewById(R.id.buttonChoice2);
         choice3 = (Button) findViewById(R.id.buttonChoice3);
@@ -31,6 +43,67 @@ public class GameActivity extends Activity implements
         choice2.setOnClickListener(this);
         choice3.setOnClickListener(this);
         choice4.setOnClickListener(this);
+    }
+
+    /*
+        @post stateCapitalMap has been filled with the fifty pairs of
+        U.S. states and their capitals
+        @throws AssertionError if data for fifty states wasn't stored
+    */
+    private void setUpStateCapitalMap() {
+        stateCapitalMap = new HashMap<String, String>();
+        stateCapitalMap.put("Alabama", "Montgomery");
+        stateCapitalMap.put("Alaska", "Juneau");
+        stateCapitalMap.put("Arizona", "Phoenix");
+        stateCapitalMap.put("Arkansas", "Little Rock");
+        stateCapitalMap.put("California", "Sacramento");
+        stateCapitalMap.put("Colorado", "Denver");
+        stateCapitalMap.put("Connecticut", "Hartford");
+        stateCapitalMap.put("Delaware", "Dover");
+        stateCapitalMap.put("Florida", "Tallahassee");
+        stateCapitalMap.put("Georgia", "Atlanta");
+        stateCapitalMap.put("Hawaii", "Honolulu");
+        stateCapitalMap.put("Idaho", "Boise");
+        stateCapitalMap.put("Illinois", "Springfield");
+        stateCapitalMap.put("Indiana", "Indianapolis");
+        stateCapitalMap.put("Iowa", "Des Moines");
+        stateCapitalMap.put("Kansas", "Topeka");
+        stateCapitalMap.put("Kentucky", "Frankfort");
+        stateCapitalMap.put("Louisiana", "Baton Rouge");
+        stateCapitalMap.put("Maine", "Augusta");
+        stateCapitalMap.put("Maryland", "Annapolis");
+        stateCapitalMap.put("Massachusetts", "Boston");
+        stateCapitalMap.put("Michigan", "Lansing");
+        stateCapitalMap.put("Minnesota", "St. Paul");
+        stateCapitalMap.put("Mississippi", "Jackson");
+        stateCapitalMap.put("Missouri", "Jefferson City");
+        stateCapitalMap.put("Montana", "Helena");
+        stateCapitalMap.put("Nebraska", "Lincoln");
+        stateCapitalMap.put("Nevada", "Carson City");
+        stateCapitalMap.put("New Hampshire", "Concord");
+        stateCapitalMap.put("New Jersey", "Trenton");
+        stateCapitalMap.put("New Mexico", "Santa Fe");
+        stateCapitalMap.put("New York", "Albany");
+        stateCapitalMap.put("North Carolina", "Raleigh");
+        stateCapitalMap.put("North Dakota", "Bismarck");
+        stateCapitalMap.put("Ohio", "Columbus");
+        stateCapitalMap.put("Oklahoma", "Oklahoma City");
+        stateCapitalMap.put("Oregon", "Salem");
+        stateCapitalMap.put("Pennsylvania", "Harrisburg");
+        stateCapitalMap.put("Rhode Island", "Providence");
+        stateCapitalMap.put("South Carolina", "Columbia");
+        stateCapitalMap.put("South Dakota", "Pierre");
+        stateCapitalMap.put("Tennessee", "Nashville");
+        stateCapitalMap.put("Texas", "Austin");
+        stateCapitalMap.put("Utah", "Salt Lake City");
+        stateCapitalMap.put("Vermont", "Montpelier");
+        stateCapitalMap.put("Virginia", "Richmond");
+        stateCapitalMap.put("Washington", "Olympia");
+        stateCapitalMap.put("West Virginia", "Charleston");
+        stateCapitalMap.put("Wisconsin", "Madison");
+        stateCapitalMap.put("Wyoming", "Cheyenne");
+        if (stateCapitalMap.size() != 50)
+            throw new AssertionError("Data for all fifty states wasn't stored");
     }
 
     @Override
